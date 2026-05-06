6051461e05b2757d37deb79561aaa0e078fdf9bd
TEK Student <TEKstudent@vlab.lan>
2026-05-06 09:38:46 -0700

    Change environment tag from dev to devlopment

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "vpc" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development-d"
+    environment = "development"
   }
 }
 
@@ -45,7 +45,7 @@ module "app_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "3.17.0"
 
-  name        = "web-sg-project-alpha-development-d"
+  name        = "web-sg-project-alpha-development"
   description = "Security group for web-servers with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
@@ -53,7 +53,7 @@ module "app_security_group" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development-d"
+    environment = "development"
   }
 }
 
@@ -61,7 +61,7 @@ module "lb_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "3.17.0"
 
-  name        = "lb-sg-project-alpha-development-d"
+  name        = "lb-sg-project-alpha-development"
   description = "Security group for load balancer with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
@@ -69,7 +69,7 @@ module "lb_security_group" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development-d"
+    environment = "development"
   }
 }
 
@@ -83,9 +83,9 @@ module "elb_http" {
   version = "2.4.0"
 
   # Ensure load balancer name is unique
-  name = "lb-${random_string.lb_id.result}-project-alpha-development-d"
+  name = "lb-${random_string.lb_id.result}-project-alpha-development"
 
-  development-dernal = false
+  internal  = false
 
   security_groups = [module.lb_security_group.this_security_group_id]
   subnets         = module.vpc.public_subnets
@@ -102,7 +102,7 @@ module "elb_http" {
 
   health_check = {
     target              = "HTTP:80/index.html"
-    development-derval            = 10
+    developmenterval            = 10
     healthy_threshold   = 3
     unhealthy_threshold = 10
     timeout             = 5
@@ -110,7 +110,7 @@ module "elb_http" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development-d"
+    environment = "development"
   }
 }
 
@@ -124,6 +124,6 @@ module "ec2_instances" {
 
   tags = {
     project     = "project-alpha",
-    environment = "development-d"
+    environment = "development"
   }
 }
